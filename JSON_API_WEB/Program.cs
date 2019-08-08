a1a4378488a0a40b3a6a0fa3393ca8a5a20c811d
Viktor Speranskyy <vencedor@ukr.net>
2019-08-08 17:48:16 +0300

    v.1

--- JSON_API_WEB/Program.cs
+++ JSON_API_WEB/Program.cs
@@ -1,15 +1,130 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace JSON_API_WEB
 {
     class Program
     {
+        public static string lastError = "";
+        public static string weather = "";//"{\"coord\":{\"lon\":30.73,\"lat\":46.48},\"weather\":[{\"id\":800,\"main\":\"Clear\",\"description\":\"ясно\",\"icon\":\"01d\"}],\"base\":\"stations\",\"main\":{\"temp\":300.15,\"pressure\":1013,\"humidity\":54,\"temp_min\":300.15,\"temp_max\":300.15},\"visibility\":10000,\"wind\":{\"speed\":10,\"deg\":190},\"clouds\":{\"all\":0},\"dt\":1565267352,\"sys\":{\"type\":1,\"id\":8915,\"message\":0.0061,\"country\":\"UA\",\"sunrise\":1565232363,\"sunset\":1565284775},\"timezone\":10800,\"id\":698740,\"name\":\"Odessa\",\"cod\":200}";
+
         static void Main(string[] args)
         {
+            // Получение данных в синхронном режиме
+            GetWeatherDataSync();
+            Console.WriteLine("Sync data load: " + lastError);
+
+            // Получение данных в асинхронном режиме
+            /*            
+            GetWeatherDataAsync().Wait();
+            Console.WriteLine("Async data load: " + lastError);
+            */
+
+            // Получение данных в асинхронном режиме
+            /*
+            GetWeatherDataAsync2();
+
+            DateTime start = DateTime.Now, now;
+            double ms = 0;
+            while (weather == "" && ms < 3000)
+            {
+                now = DateTime.Now;
+                ms = (now - start).TotalMilliseconds;
+                Console.SetCursorPosition(0, 2);
+                Console.Write($"Async data load: {(int)ms} ms");
+            }
+            if (weather == "")
+            {
+                Console.SetCursorPosition(15, 2);
+                Console.Write(" request timout\n" + lastError);
+            }
+            else
+            {
+                Console.WriteLine($"Async weather data load: successfuly");
+                Console.WriteLine("\nPress any key to continue...");
+                Console.ReadKey();
+            }
+            */
+
+            Console.WriteLine(weather);
+            if (weather != "")
+            {
+//                JObject details = JObject.Parse(weather);
+//                Console.WriteLine(details);
+//                Console.WriteLine(details.GetValue("sys"));
+//                Console.WriteLine(details.GetValue("sys").Value<String>("country"));
+
+//                WeatherInfo currentWeather = JsonConvert.DeserializeObject<WeatherInfo>(weather);
+//                Console.WriteLine($"{currentWeather.Name}({currentWeather.Coord.Lat} - {currentWeather.Coord.Lon})");
+            }
+            else
+            {
+                Console.WriteLine("No weather data available");
+            }
+        }
+
+        private static void GetWeatherDataSync()
+        {
+            lastError = "";
+            WebClient client = new WebClient();
+            try
+            {
+                using (Stream stream = client.OpenRead("https://api.openweathermap.org/data/2.5/weather?q=Odessa,ua&appid=dac392b2d2745b3adf08ca26054d78c4&lang=ru"))
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        weather = reader.ReadToEnd();
+                    }
+                }
+                lastError = "successfuly";
+            }
+            catch (Exception e)
+            {
+                lastError = e.Message;
+            }
+        }
+
+        private static async Task GetWeatherDataAsync()
+        {
+            lastError = "";
+            try
+            {
+                WebClient client = new WebClient();
+                StreamReader reader = new StreamReader(await client.OpenReadTaskAsync(new Uri("https://api.openweathermap.org/data/2.5/weather?q=Odessa,ua&appid=dac392b2d2745b3adf08ca26054d78c4&lang=ru", UriKind.Absolute)));
+                weather = reader.ReadLine();
+                lastError = "successfuly";
+            }
+            catch (Exception e)
+            {
+                lastError = e.Message;
+            }
+        }
+
+        private static void GetWeatherDataAsync2()
+        {
+            lastError = "";
+            WebClient client = new WebClient();
+
+            try
+            {
+                client.OpenReadCompleted += client_OpenReadCompleted;
+                client.OpenReadAsync(new Uri("https://api.openweathermap.org/data/2.5/weather?q=Odessa,ua&appid=dac392b2d2745b3adf08ca26054d78c4&lang=ru"));
+
+                void client_OpenReadCompleted(object sender, OpenReadCompletedEventArgs e)
+                {
+                    if (e.Error == null)
+                        weather = new StreamReader(e.Result).ReadToEnd().ToString();
+                }
+            }
+            catch (Exception e)
+            {
+                lastError = e.Message;
+            }
         }
     }
 }
---
+++ JSON_API_WEB/Weather.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace JSON_API_WEB
+{
+    public partial class WeatherInfo
+    {
+        public Coord Coord { get; set; }
+        public Weather[] Weather { get; set; }
+        public string Base { get; set; }
+        public Main Main { get; set; }
+        public long Visibility { get; set; }
+        public Wind Wind { get; set; }
+        public Clouds Clouds { get; set; }
+        public long Dt { get; set; }
+        public Sys Sys { get; set; }
+        public long Timezone { get; set; }
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public long Cod { get; set; }
+    }
+
+    public partial class Clouds
+    {
+        public long All { get; set; }
+    }
+
+    public partial class Coord
+    {
+        public double Lon { get; set; }
+        public double Lat { get; set; }
+    }
+
+    public partial class Main
+    {
+        public double Temp { get; set; }
+        public long Pressure { get; set; }
+        public long Humidity { get; set; }
+        public double TempMin { get; set; }
+        public double TempMax { get; set; }
+    }
+
+    public partial class Sys
+    {
+        public long Type { get; set; }
+        public long Id { get; set; }
+        public double Message { get; set; }
+        public string Country { get; set; }
+        public long Sunrise { get; set; }
+        public long Sunset { get; set; }
+    }
+
+    public partial class Weather
+    {
+        public long Id { get; set; }
+        public string Main { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+
+    public partial class Wind
+    {
+        public long Speed { get; set; }
+        public long Deg { get; set; }
+    }
+}
