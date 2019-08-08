b41e953476b65bb2a803b96bf8f20912d17d7e55
Viktor Speranskyy <vencedor@ukr.net>
2019-08-08 13:58:51 +0300

    Add project files.

---
+++ JSON_API_WEB/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSON_API_WEB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
